8dc0251c6379fe5de0368579c9f67c6331674e6d
Himalaya <yugmani@yahoo.com>
2022-02-20 01:06:26 -0800

    movie-image layout

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 const api_key = "046a7526cffd553aec9f3c6d073442a5";
 const APIURL = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=free`;
 
-const IMGPATH = "https://image.tmdb.org/t/p/w500";
+const IMGPATH = "https://image.tmdb.org/t/p/w500/";
+const mainEl = document.querySelector("main");
+
 async function getMovies() {
   const resp = await fetch(APIURL);
   const respData = await resp.json();
   console.log(respData);
 
   respData.results.forEach((movie) => {
-    const img = document.createElement("img");
-    img.src = IMGPATH + movie.poster_path;
-    document.body.appendChild(img);
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
+    <img
+    src="${IMGPATH}${movie.poster_path}";
+    alt=""
+  />
+  <div class="movie-info">
+    <h3>${movie.title}</h3>
+    <span class="${getClassByRate(movie.vote_average)}">${
+      movie.vote_average
+    }</span>
+  </div>
+ `;
+
+    mainEl.appendChild(movieEl);
   });
 
   return respData;
 }
 
 getMovies();
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
